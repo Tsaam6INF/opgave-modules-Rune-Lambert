d0b6cf303aa0c8be81eea46006e22bcdd7897dd6
Rune-Lambert <Rune.Lambert@student.tsaam.be>
2024-09-13 10:22:40 +0200

    oplossing opgave optellen

---
+++ optellen.js
@@ -0,0 +1,12 @@
+const optellen = (getal1, getal2)=>{
+    return getal1 + getal2;
+}
+
+const aftrekken = (getal1, getal2)=>{
+    return getal1- getal2
+}
+
+module.exports = {
+    optellen,
+    aftrekken,
+};
